973bd7127e2bcd9da346168f884ebe7d3faccc12
Alvaro <alvarofajardoplaza@MBP-de-Alvaro.home>
2023-05-20 17:01:47 +0200

    añadimos libreria MUI y primera prueba de uso de MUI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import React from 'react';
+import { Container, Paper, Typography, Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/system';
+
+/* A partir de aquí vamos a crear nuestro tema.
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#ff4400',
+      //light, dark, contrastText --- estos son los 4 parámetros dentro de palette-primary.
+    }
+  }
+});
+*/
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Buenas!</p>
-    </div>
+      <Container sx= {{
+        p: '2rem'
+      }}>
+          <Typography variant='h4'>Bienvenido a la App</Typography>
+          <Typography variant='body1'>Se trata de una app de domótica para el hogar.</Typography>
+      </Container>
   );
 }
 
