6a78be4fcf2a9a9b948abf25a93ed68879308252
Alvaro <alvarofajardoplaza@MBP-de-Alvaro.home>
2023-06-03 11:41:27 +0200

    modificaciones sobre el modelo User para adaptar funcionalidades de registro de usuario en UserController. Sprint viernes 2 de junio

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
 
+
 require('dotenv').config();
 
 const mySecret = process.env.TOKENSECRET;
@@ -40,24 +41,46 @@ const registerNewUser = async (req, res) => {
 
     newUser.token = token;
     newUser.password = undefined;
+    // users.push(newUser);
 
-    res.status(200).send({msg:'¡Usuario creado correctamente!'})
+    res.status(200).send({msg:'¡Usuario creado correctamente!', token})
     } catch(error) {
         console.log(error)
     }
 };
 
+//Necesitamos crear una función que nos devuelva un array de objetos. Cada objeto es un usuario.
+//Este endpoint nos va a servir para llamar a todos los usuarios.
+const getUsers = (req, res) => {
+    User.find()
+    .then(userDocs => {
+        if(userDocs.length === 0) {
+            res.status(404).send({msg: "No se han encontrado usuarios."})
+        } else {
+            console.log('Estos son mis usuarios: ', userDocs);
+            return userDocs
+        }
+    })
+    .catch(error => res.status(400).send(error))
+};
+
+
+
 const loginUser = async (req, res) => {
     try {
         //1º Cogemos todos los datos del front con req.body
         const { email, password } = req.body;
+        console.log('mi req body es: ', req.body);
         if(!(email && password)){
             return res.status(400).send({msg:'Para acceder tienes que introducir tu email y tu contraseña de usuario.'})
         };
         //2º validamos y 3º Encontramos al ususario en la BD
         //Comprobamos que la contraseña que el usuario nos ha enviado es la correcta --- bcrypt nos ofrece facilidades para hacerlo.
-        const userFound = await User.find( user => user.email === 'email')
-        if (userFound && (await bcrypt.compare(password, User.password))) {
+
+        //Llamar a nuestro array de ussuarios y encontrar el usuario que está haciendo login.
+        const userFound = await User.findOne( { email: email } )
+        console.log('el usuario encontrado es: ', userFound);
+        if (userFound && (await bcrypt.compare(password, userFound.password))) {
             //4º Si se cumple la condición de que existe el usuario y la contraseña es correcta -> Enviamos el token para este usuario.
             const token = jwt.sign(
                 {id: userFound._id, //el campo _id que nos facilita mongoose.
@@ -66,53 +89,43 @@ const loginUser = async (req, res) => {
                 { expiresIn: '2h'} //propiedad para expirar el token en 2 horas.
             );
             userFound.token = token
-            userFound.password = undefined
+            //userFound.password = undefined
 
             //Una vez tenemos todos estos pasos, enviamos la cookie para el usuario en el token. Sección de la cookie a continuación.
             const options = {
                     expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
                     httpOnly: true,
             };
-            res.status(200).cookie('token', token, options).send({success: true}, token, User)
-        }
+            return res.status(200).cookie('token', token, options).send({success: true})
+        } else { return res.status(400).send({msg: 'Usuario o contraseña incorrectos.'}) }
     } catch(error) {
-        console.log(error)
+        console.log(error);
+        return res.status(400).send({msg: 'Usuario o contraseña incorrectos.'})
     }
 };
 
-/* CÓDIGO ESCRITO EL SÁBADO --- ÁLVARO
-//Vamos a crear un nuevo usuario cuyos datos nos llegarán desde el formulario de registro del front.
-const addNewUser = (req, res, next) => {
-    const { name, email, password } = req.body;
-    console.log('Soy el POST que registra un nuevo usuario!')
 
-    const newUser = { email, password };
-    users.create(newUser);
+//Este endpoint nos va a servir para llamarnos a nosotros mismos. Es el endpoint de "mi perfil"
+const myUser = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1]
+    console.log('token es: ', token);
+    //Ahora, decodificamos el token usando la función .verify!
+    try{
+        const decodedToken = jwt.verify(token, mySecret)
+        console.log('El token decodificado es: ', decodedToken);
+        res.status(200).send({ result: 'success', data: decodedToken })
+    } catch(error) {
+        console.log('Hay un error al tratar de verificar el token.')
+        res.status(400).send({msg: 'No es un token válido.'})
+    }
 
-    //Ahora creamos el token para este nuevo usuario
-    const token = jwt.sign({name, email}, mySecret, {expiresIn: '1h'})
-    res.status(200).send({msg: 'ok', token });
 };
 
-//ahora el endpoint para hacer login en un determinado usuario ya creado.
-const loginUser = (req, res, next) => {
-    const { email, password } = req.body;
-    console.log('Soy el POST que logea un usuario!');
-
-    //Tenemos que referirnos al usuario ya existente en nuestra BD.
-    const userFoundInDB = users.find( user => user.email === 'email')
-
-    if(!userFoundInDB){ return res.status(400).send({msg: 'El usuario no existe. Debes registrarte como usuario.'})};
-
-    if(userFoundInDB.password !== password) {return res.status(400).send({msg: 'La contraseña no es correcta.'})};
-
-    const token = jwt.sign({name: userFoundInDB.name, email}, mySecret,{expiresIn: '1h'});
-    return res.status(200).send({msg: 'ok', token });
-}
-*/
 
 module.exports = {
     registerNewUser,
-    loginUser
+    loginUser,
+    getUsers,
+    myUser,
 };
 
