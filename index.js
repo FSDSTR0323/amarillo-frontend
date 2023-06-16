a0afc24e8f450e3a0a134da643cf6aa643f34870
Jordi Galobart Salvat <jgalobart@gmail.com>
2023-06-16 21:10:08 +0200

    test conflicto 33

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 9000;
 // test conflicto jgalobart
-//Hola, vamos a crear un conflicto 2 ventidos.
+//Hola, vamos a crear un conflicto 3 trentaitres.
 //Modificación Alvaro.
 app.use(express.json());
 app.use(cors());
