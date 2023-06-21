ae0a1b9b3a18fa40abea1b9f6416f6b39207feb4
RaquelJaldo <128419382+RaquelJaldo@users.noreply.github.com>
2023-06-21 20:13:53 +0200

    modifico houseController

--- src/models/houseModel.js
+++ src/models/houseModel.js
@@ -37,6 +37,11 @@ const houseSchema = new Schema({
     size: {
         type: Number
     },
+    roomsNumber: {
+        type: Number,
+        minLength: 1,
+        maxLength: 20
+    },
     userId:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
@@ -49,67 +54,3 @@ const houseSchema = new Schema({
 module.exports = mongoose.model('House', houseSchema);
 
 
-
-const App = () => {
-  const [inputFields, setInputFields] = useState([]);
-
-  const addInputFields = () => {
-    setInputFields([
-      ...inputFields,
-      { nombre: '', direccion: '', tamaño: '', fecha: '' },
-    ]);
-  };
-
-  const handleInputChange = (index, event) => {
-    const { name, value } = event.target;
-    const values = [...inputFields];
-    
-    // Validar longitud máxima del campo "nombre"
-    if (name === 'nombre' && value.length > 20) {
-      return; // No permitir más caracteres si se alcanza la longitud máxima
-    }
-
-    values[index][name] = value;
-    setInputFields(values);
-  };
-
-  return (
-    <div>
-      <button onClick={addInputFields}>Agregar campos</button>
-      {inputFields.map((inputField, index) => (
-        <div key={index}>
-          <input
-            type="text"
-            name="nombre"
-            value={inputField.nombre}
-            onChange={(event) => handleInputChange(index, event)}
-            placeholder="Nombre"
-          />
-          <input
-            type="text"
-            name="direccion"
-            value={inputField.direccion}
-            onChange={(event) => handleInputChange(index, event)}
-            placeholder="Dirección"
-          />
-          <input
-            type="text"
-            name="tamaño"
-            value={inputField.tamaño}
-            onChange={(event) => handleInputChange(index, event)}
-            placeholder="Tamaño"
-          />
-          <input
-            type="text"
-            name="fecha"
-            value={inputField.fecha}
-            onChange={(event) => handleInputChange(index, event)}
-            placeholder="Fecha"
-          />
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default App;
