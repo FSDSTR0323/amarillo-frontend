3de0a2859c48eb47f6c5b0c31fc17a09775bb1b5
samuelnarioSOREVAN <samuel.nario@sorevan.com>
2023-06-23 20:40:25 +0200

    nno changes

--- src/componentes/MenuBars/HubNavBar.js
+++ src/componentes/MenuBars/HubNavBar.js
@@ -7,22 +7,18 @@ import { Typography, Button, IconButton, Box, AppBar, Toolbar } from '@mui/mater
 
 //ICONOS
 import HomeIcon from '@mui/icons-material/Home';
-<<<<<<< HEAD
 import { HomeMax } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
-=======
 import PersonIcon from '@mui/icons-material/Person';
 import TuneIcon from '@mui/icons-material/Tune';
 import LogoutIcon from '@mui/icons-material/Logout';
 
->>>>>>> fc195470953493495eb7244931d0c229e587b614
-
 const HubNavBar = () => {
 
   //Creamos la función para el logOut -> NOS LA TRAEMOS DE AUTHCONTEXT:
-  // const logUserOut = () => {
-  // window.localStorage.clear();
-  // }
+  const logUserOut = () => {
+  window.localStorage.clear();
+  }
 
     return (
     <Box sx={{ display: 'flex' }}>
