90c291b0dda2821c749228641463c4c83a032b68
RaquelJaldo <128419382+RaquelJaldo@users.noreply.github.com>
2023-07-19 21:32:27 +0200

    editar usuario, revisar

--- src/componentes/containers/MyUserPanel.js
+++ src/componentes/containers/MyUserPanel.js
@@ -21,8 +21,6 @@ const MyUserPanel = ({name, surname, birthYear, phoneNumber, email}) => {
     // avatar: '',
   });
 
-
-
   const [editedUserData, setEditedUserData] = useState({
     name: '',
     surname: '',
@@ -87,26 +85,26 @@ const MyUserPanel = ({name, surname, birthYear, phoneNumber, email}) => {
   //   setEditMode(false);
   // }
 
-    const handleSaveChanges = async () => {
-      try {
-        const response = await fetch(`/editar-usuario/${userData._id}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(editedUserData),
-        });
+    // const handleSaveChanges = async () => {
+    //   try {
+    //     const response = await fetch(`/editar-usuario/${userData._id}`, {
+    //       method: 'PUT',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify(editedUserData),
+    //     });
     
-        if (response.ok) {
-          console.log('Datos actualizados correctamente');
-          // Puedes realizar alguna acción adicional después de actualizar los datos, como mostrar un mensaje de éxito o redirigir al usuario a otra página.
-        } else {
-          console.error('Error al actualizar los datos');
-        }
-      } catch (error) {
-        console.error('Error al realizar la solicitud:', error);
-      }
-    }
+    //     if (response.ok) {
+    //       console.log('Datos actualizados correctamente');
+    //       // Puedes realizar alguna acción adicional después de actualizar los datos, como mostrar un mensaje de éxito o redirigir al usuario a otra página.
+    //     } else {
+    //       console.error('Error al actualizar los datos');
+    //     }
+    //   } catch (error) {
+    //     console.error('Error al realizar la solicitud:', error);
+    //   }
+    // }
   
 
   const envioForm = async ({name, surname, email, phoneNumber, birthYear}) =>{
@@ -141,189 +139,20 @@ const MyUserPanel = ({name, surname, birthYear, phoneNumber, email}) => {
               boxShadow: '4px 8px 8px -4px rgb(202, 213, 216)',
             }}
           >
-            <Stack spacing={2} width={600}>
-              <label htmlFor="avatar-input">
-                <Box sx={{ position: 'relative' }}>
-                  <Avatar
-                    alt="HomeHub Avatar"
-                    src={editedUserData.avatar ? URL.createObjectURL(editedUserData.avatar) : ''}
-                    sx={{ width: 80, height: 80 }}
-                  />
-                   <Typography variant='body1' sx={{color:'#505050', paddingTop:'1rem'}}>Elige una imagen para tu perfil:</Typography>
-
-<Box sx={{display:'flex', gap:'1rem'}}>
-    <Button
-        variant="info"
-        component="label"
-        >
-        Seleccionar una imagen de perfil
-            <input
-                type="file"
-                hidden
-                onChange={(event) => {
-                    setFile(event.target.files[0])
-                }}
-            />
-    </Button>
-    <Button variant='contained'
-      color='success'
-      onClick={uploadImage}
-      startIcon={<SaveIcon/>}
-      sx={{}}></Button>
-</Box>
-
-<Typography sx={{textAlign:'center', paddingTop:'1rem', color:'#AEAEAE'}}>___________</Typography>
-
-<Box sx={{display: 'flex', flexDirection: 'row', gap:'1rem', justifyContent: 'center', paddingTop:'1rem', paddingBottom:'1rem'}}>
-    <Button type='cancel' variant='outlined' color='info' onClick={() => closePopUp(false)}>
-        Cancelar
-    </Button>
-    <Button type='submit' value ="" variant='contained' color='primary'>
-        Actualizar datos de usuario
-    </Button>
-</Box>
-</Box>
-                  <Box
-                    sx={{
-                      position: 'absolute',
-                      bottom: 0,
-                      right: 0,
-                      bgcolor: 'rgba(0, 0, 0, 0.6)',
-                      color: 'white',
-                      fontSize: '10px',
-                      padding: '2px',
-                    }}
-                  >
-                  </Box>
-      
-                <input
-                  id="avatar-input"
-                  type="file"
-                  accept="image/*"
-                  style={{ display: 'none' }}
-                  onChange={handleFileInputChange}
-                />
-              </label>
-
-              {editMode ? (
-                <form onSubmit={handleSubmit(envioForm)}>
-                  <TextField
-                    label="Nombre/Name"
-                    variant="filled"
-                    // fullWidth
-                    name="name"
-                    value={name}
-                    // onChange={handleInputChange}
-                    // placeholder="Introduce tu nombre"
-                    // InputProps={{
-                    //   style: { backgroundColor: 'rgba(255, 255, 255, 0.5)' },
-                    // }}
-                    {...register("name")}
-                  />
-                  <TextField
-                    label="Apellido/Surname"
-                    variant="filled"
-                    // fullWidth
-                    name="surname"
-                    value={surname}
-                    // onChange={handleInputChange}
-                    // placeholder="Introduce tu apellido"
-                    // InputProps={{
-                    //   style: { backgroundColor: 'rgba(255, 255, 255, 0.5)' },
-                    // }}
-                    {...register("surname")}
-                  />
-                  <TextField
-                    label="Email"
-                    variant="filled"
-                    fullWidth
-                    name="email"
-                    value={editedUserData.email}
-                    onChange={handleInputChange}
-                    placeholder="Introduce tu e-mail"
-                    InputProps={{
-                      style: { backgroundColor: 'rgba(255, 255, 255, 0.5)' },
-                    }}
-                  />
-                  <PhoneInput
-                    label="Teléfono de contacto"
-                    variant="filled"
-                    fullWidth
-                    name="phoneNumber"
-                    value={editedUserData.phoneNumber}
-                    onChange={(phone) => setEditedUserData({ ...editedUserData, phoneNumber: phone })}
-                    placeholder="Introduce tu número de teléfono"
-                    inputStyle={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}
-                    countryCodeEditable
-                    country={'auto'}
-                    disableDropdown={false}
-                    dropdownClass="react-phone-input-2-dropdown"
-                    inputProps={{
-                      maxLength: 15,
-                    }}
-                  />
-                  <TextField
-                    label="Año de nacimiento/BirthYear"
-                    variant="filled"
-                    // fullWidth
-                    name="birthYear"
-                    value={birthYear}
-                    // onChange={handleInputChange}
-                    // placeholder="Introduce tu año de nacimiento"
-                    // InputProps={{
-                    //   style: { backgroundColor: 'rgba(255, 255, 255, 0.5)' },
-                    // }}
-                    {...register("birthYear")}
-                  />
-
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      gap: '1rem',
-                      justifyContent: 'center',
-                      paddingTop: '2rem',
-                      paddingBottom: '1rem',
-                    }}
-                  >
-                    <Button type="submit" variant="contained" color="primary" 
-                      onClick={() => {
-                                      console.log ("Guardamos?? ", editedUserData )
-                                      setEditMode(false)
-                                    }}
-                                    >
-                      Guardar
-                    </Button>
-                    <Button variant="outlined" color="info" onClick={() => setEditMode(false)}>
-                      Cancelar
-                    </Button>
-                  </Box>
-                </form>
-              ) : (
-                <>
-                  <Typography>{userData.name}</Typography>
-                  <Typography>{userData.surname}</Typography>
-                  <Typography>{userData.email}</Typography>
-                  <Typography>{userData.phoneNumber}</Typography>
-                  <Typography>{userData.birthYear}</Typography>
-
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      gap: '1rem',
-                      justifyContent: 'center',
-                      paddingTop: '2rem',
-                      paddingBottom: '1rem',
-                    }}
-                  >
-                    <Button variant="outlined" color="info" onClick={handleEditClick}>
-                      Editar mis datos
-                    </Button>
-                  </Box>
-                </>
-              )}
-            </Stack>
+          {
+            {userData}?
+              <Box>
+                <Typography>
+                {userData.name}
+                </Typography>
+                <Typography>
+                {userData.email} 
+                </Typography>
+              </Box> 
+            :null           
+          }
+          //imprimir el formulario q es copiar la info que tenemos en add room form q nos manda una nueva hab. copiarla aqui y cuando se manda al backend volver a recibir la info de estos datos q estamos pintando. 
+            
           </Box>
         </Box>
       </Box>
